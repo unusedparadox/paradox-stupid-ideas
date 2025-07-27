eb20429b895b4e564643715d0cbbd883bc66b581
unusedparadox <unusedparadox@gmail.com>
2025-07-26 18:28:27 -0600

    Made Garage Fork more compatible with other mods (and eliminated manual crossmod)

--- content/jokers/2 - uncommon/garagefork.lua
+++ content/jokers/2 - uncommon/garagefork.lua
@@ -20,110 +20,21 @@ SMODS.Joker{
 		return {vars = {new_numerator, new_denominator}}
 	end
 }
-SMODS.Booster:take_ownership_by_kind('Arcana', {
-        create_card = function(self, card, i)
-            local _card, spect_fork
-            local spect_omen = (G.GAME.used_vouchers.v_omen_globe and pseudorandom('omen_globe') > 0.8)
-            local silly = G.GAME.used_vouchers.v_fmod_circus and pseudorandom("circus") > 0.8
-            for _, v in ipairs(SMODS.find_card("j_para_garagefork", false)) do
-                if SMODS.pseudorandom_probability(card, 'para_garagefork', v.ability.extra.numerator,
-                v.ability.extra.denominator, 'para_garagefork') then
-                    spect_fork = true
-                    break
-                end
+local createcardold = create_card
+create_card = function(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
+    local spect_fork
+    if key_append == "ar1" or key_append == "pl1" then
+        for _, v in ipairs(SMODS.find_card("j_para_garagefork", false)) do
+            if SMODS.pseudorandom_probability(card, 'para_garagefork', v.ability.extra.numerator,
+            v.ability.extra.denominator, 'para_garagefork') then
+                spect_fork = true
+                break
             end
-            if spect_fork then
-                _card = {
-                    set = "Spectral",
-                    area = G.pack_cards,
-                    skip_materialize = true,
-                    soulable = true,
-                    key_append =
-                    "para_spectral_arcana_fork"
-                }
-            elseif silly then
-                _card = {
-                    set = "Silly",
-                    area = G.pack_cards,
-                    skip_materialize = true,
-                    soulable = true,
-                    key_append = "sil1"
-                }
-            elseif spect_omen then
-                _card = {
-                    set = "Spectral",
-                    area = G.pack_cards,
-                    skip_materialize = true,
-                    soulable = true,
-                    key_append =
-                    "ar2"
-                }
-            else
-                _card = {
-                    set = "Tarot",
-                    area = G.pack_cards,
-                    skip_materialize = true,
-                    soulable = true,
-                    key_append =
-                    "ar1"
-                }
-            end
-            return _card
-        end,
-    },
-    true
-)
-SMODS.Booster:take_ownership_by_kind('Celestial', {
-        create_card = function(self, card, i)
-            local _card, spect, scope, _planet
-            if (G.GAME.used_vouchers.v_telescope) and i == 1 then
-                local _hand = PSI.mostplayedhand(false)[1]
-                for _, v in pairs(G.P_CENTER_POOLS.Planet) do
-                    if v.config.hand_type == _hand then
-                        _planet = v.key
-                    end
-                end
-                if _planet then scope = true end
-            end
-            if not scope then
-                for _, v in ipairs(SMODS.find_card("j_para_garagefork", false)) do
-                    if SMODS.pseudorandom_probability(card, 'para_garagefork', v.ability.extra.numerator,
-                    v.ability.extra.denominator, 'para_garagefork') then
-                        spect = true
-                        break
-                    end
-                end
-            end
-            if scope then
-                _card = {
-                    set = "Planet",
-                    area = G.pack_cards,
-                    skip_materialize = true,
-                    soulable = true,
-                    key = _planet,
-                    key_append = "pl1"
-                }
-            elseif spect then
-                _card = {
-                    set = "Spectral",
-                    area = G.pack_cards,
-                    skip_materialize = true,
-                    soulable = true,
-                    key_append =
-                    "ar2"
-                }
-            else
-                _card = {
-                    set = "Planet",
-                    area = G.pack_cards,
-                    skip_materialize = true,
-                    soulable = true,
-                    key_append =
-                    "ar1"
-                }
-            end
-            return _card
-        end,
-    },
-    true
-)
+        end
+    end
+    if spect_fork then
+        return createcardold("Spectral", area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
+    else
+        return createcardold(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
+    end
+end
